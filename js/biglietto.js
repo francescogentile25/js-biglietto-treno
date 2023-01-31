86d30ece4bf157c53ba693cde0c0bc77f697eafa
francescogentile25 <francescogentile250893@gmail.com>
2023-01-31 15:09:30 +0100

    START to cry, goodbye Riccardo, we will miss you!

---
+++ js/biglietto.js
@@ -0,0 +1,4 @@
+//Chiedere numero km
+//Chiedere età passeggero
+// definire costo per km e moltiplicarlo per i km che deve fare il passegero
+//  applicare socnto in base all'età del passegero  20% per i minorenni 40% per gli over 65.
